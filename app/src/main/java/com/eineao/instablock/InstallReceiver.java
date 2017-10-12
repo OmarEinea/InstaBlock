21e19c80e3dcb9cdd575e249f42a78a0b172f599
Omar Einea <eineao@gmail.com>
2017-10-12 23:35:43 +0400

    Import RootTools & Use It To Uninstall Any Package Silently

--- app/src/main/java/com/eineao/instablock/InstallReceiver.java
+++ app/src/main/java/com/eineao/instablock/InstallReceiver.java
@@ -3,7 +3,11 @@ package com.eineao.instablock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.stericson.RootShell.execution.Command;
+import com.stericson.RootTools.RootTools;
 
 /**
  * Created by Omar on 9/27/2017.
@@ -17,8 +21,21 @@ public class InstallReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // If the broadcast received is due to an app installation
         if(intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
-            // Show a toast to indicate that for now
-            Toast.makeText(context, "An app has been installed", Toast.LENGTH_LONG).show();
+            // Uninstall any app after installation (for now)
+            uninstallPackage(intent.getDataString().substring(8));
+            // Show a toast to indicate that app was uninstalled
+            Toast.makeText(context, "InstaBlock uninstalled an app", Toast.LENGTH_LONG).show();
         }
+    }
+
+    // A function that silently uninstalls a package using root access
+    public static void uninstallPackage(String packageName) {
+        // Make sure root access is granted
+        if(RootTools.isAccessGiven())
+            try { // Try uninstalling the package using PackageManager's "uninstall" command
+                RootTools.getShell(true).add(new Command(0, "pm uninstall " + packageName));
+            } catch (Exception e) { // If command execution fails, record an error of failure
+                Log.e("uninstallPackage()", "Failed to execute uninstall package command");
+            }
     }
 }
