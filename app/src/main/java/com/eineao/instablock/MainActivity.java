f518d2702fb5e152228c8c94435b335ee63fabd2
Omar Einea <eineao@gmail.com>
2017-10-31 00:35:40 +0400

    Replace Fab Button With A Custom One With Multiple Choices

--- app/src/main/java/com/eineao/instablock/MainActivity.java
+++ app/src/main/java/com/eineao/instablock/MainActivity.java
@@ -1,10 +1,7 @@
 package com.eineao.instablock;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private final int SEARCH = 1;
@@ -13,14 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivityForResult(new Intent(MainActivity.this, SearchActivity.class), SEARCH);
-            }
-        });
     }
-
 }
