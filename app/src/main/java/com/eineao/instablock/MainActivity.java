5a16b2ee20df219339ab3aa66cb915f6ee9d9505
Omar Einea <eineao@gmail.com>
2017-10-31 10:20:45 +0400

    Linked Play Store Button & Made FAB Close When NoT In Focus

--- app/src/main/java/com/eineao/instablock/MainActivity.java
+++ app/src/main/java/com/eineao/instablock/MainActivity.java
@@ -1,14 +1,54 @@
 package com.eineao.instablock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 public class MainActivity extends AppCompatActivity {
     private final int SEARCH = 1;
+    private View mFabShade;
+    private RelativeLayout mMainLayout;
+    private FloatingActionsMenu mFabMenu;
+    private FloatingActionButton mPlayStoreButton, mInstalledAppsButton, mPackageNameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFabMenu = findViewById(R.id.fab_menu);
+        mFabShade = findViewById(R.id.fab_shade);
+        mPlayStoreButton = findViewById(R.id.play_store_button);
+
+        mFabMenu.getChildAt(3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFabMenu.toggle();
+                mFabShade.setVisibility(
+                        mFabMenu.isExpanded() ? View.VISIBLE : View.INVISIBLE
+                );
+            }
+        });
+
+        mFabShade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFabMenu.toggle();
+                mFabShade.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        mPlayStoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, SearchActivity.class), SEARCH);
+                mFabMenu.collapse();
+            }
+        });
     }
 }
