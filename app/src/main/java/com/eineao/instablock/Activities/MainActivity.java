fb76edca96c852880e74806da322e05d88751a6b
kadirkid <kadirkid14@outlook.com>
2017-12-02 16:40:08 +0400

    Added PIN

--- app/src/main/java/com/eineao/instablock/Activities/MainActivity.java
+++ app/src/main/java/com/eineao/instablock/Activities/MainActivity.java
@@ -1,20 +1,30 @@
 package com.eineao.instablock.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.eineao.instablock.Fragments.BlockedAppsFragment;
 import com.eineao.instablock.Fragments.FiltersFragment;
+import com.eineao.instablock.PrefUtils;
 import com.eineao.instablock.R;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
@@ -24,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionsMenu mFabMenu;
     private FloatingActionButton mPlayStoreButton, mInstalledAppsButton;
     private ViewPager mViewPager;
+    private String pin;
+    private PrefUtils pref;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mSharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = findViewById(R.id.container);
@@ -69,6 +84,114 @@ public class MainActivity extends AppCompatActivity {
 
         mPlayStoreButton.setOnClickListener(getSearchAppsOnClickListener(PlayStoreActivity.class));
         mInstalledAppsButton.setOnClickListener(getSearchAppsOnClickListener(InstalledAppsActivity.class));
+
+        pref = new PrefUtils(this);
+        pref.clearPin();
+        pin = pref.getPin();
+        if(pin.equals("")){
+            newPin();
+        }
+        else{
+            promptPin();
+        }
+
+    }
+
+    private void newPin() {
+        LayoutInflater li = LayoutInflater.from(getApplicationContext());
+        View promptsView = li.inflate(R.layout.new_pin_dialog, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                new ContextThemeWrapper(this, R.style.myDialog)
+        );
+
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText newPin = promptsView.findViewById(R.id.new_pin);
+        final EditText confirmPin = promptsView.findViewById(R.id.confirm_pin);
+
+        final Button cancel = promptsView.findViewById(R.id.n_cancel);
+        final Button enter = promptsView.findViewById(R.id.n_enter);
+
+        alertDialogBuilder.setCancelable(false);
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        final AlertDialog finalDialog = alertDialog;
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finalDialog.dismiss();
+                finish();
+            }
+        });
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String new_pin = newPin.getText().toString().trim();
+                final String confirm_pin = confirmPin.getText().toString().trim();
+
+                if(confirm_pin.equals(new_pin)){
+                    pref.setPin(new_pin);
+                    finalDialog.dismiss();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "The PINs do not match",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // show it
+        alertDialog.show();
+    }
+
+    private void promptPin() {
+        LayoutInflater li = LayoutInflater.from(getApplicationContext());
+        View promptsView = li.inflate(R.layout.enter_pin_dialog, null);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                new ContextThemeWrapper(this, R.style.myDialog)
+        );
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView.findViewById(R.id.pin);
+
+        Button enter = promptsView.findViewById(R.id.e_enter);
+        Button cancel = promptsView.findViewById(R.id.e_cancel);
+
+        alertDialogBuilder.setCancelable(false);
+
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        final AlertDialog finalDialog = alertDialog;
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finalDialog.dismiss();
+                finish();
+            }
+        });
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(pin.equals(userInput.getText().toString())) {
+                    Log.i("SHAREDPREF", pin);
+                    Toast.makeText(getApplicationContext(), "Welcome",
+                            Toast.LENGTH_SHORT).show();
+                    finalDialog.dismiss();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong PIN",
+                            Toast.LENGTH_SHORT).show();
+                    finalDialog.dismiss();
+                    finalDialog.show();
+                    userInput.setText("");
+                }
+            }
+        });
+
+        // show it
+        alertDialog.show();
     }
 
     private View.OnClickListener getSearchAppsOnClickListener(final Class targetActivity) {
